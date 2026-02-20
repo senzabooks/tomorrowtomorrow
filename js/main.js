935ef696692be867ddf863df981ca56f8108657e
Stanislaw Zielinski <vladyslav.omelyanenko@gmail.com>
2026-02-20 03:18:10 +0100

    add app height function

--- js/main.js
+++ js/main.js
@@ -11,3 +11,17 @@ console.log("loaded columns:", cols.length);
 
 attachAutosaveHandlers(cols);
 mountAddColumnTile();
+
+function bindAppHeightToVisualViewport() {
+  const vv = window.visualViewport;
+  const set = () => {
+    const h = vv ? vv.height : window.innerHeight;
+    document.documentElement.style.setProperty("--app-height", `${h}px`);
+  };
+  set();
+  vv?.addEventListener("resize", set);
+  vv?.addEventListener("scroll", set);
+  window.addEventListener("orientationchange", set);
+}
+
+bindAppHeightToVisualViewport();
